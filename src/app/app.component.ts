38f45d988139b74f9d289db46847343d6037d0f8
25657g <gayathmag@gmail.com>
2022-12-11 18:19:24 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  host: { class: 'l-app' },
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'eg-components';
+
+  checkboxOptions= ["Ford", "BMW", "Fiat"]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderModule} from './header/header.module';
+import { FormFieldComponent } from './form-field/form-field.component';
+import { InputRefDirective } from './form-field/input-ref.directive';
+import {IconModule} from './icon/icon.module';
+import {DirectiveModule} from './button/directive.module';
+import {AvatarModule} from './avatar/avatar.module';
+import {DividerModule} from './divider/divider.module';
+import {CheckboxModule} from './checkbox/checkbox.module';
+import {BadgeModule} from './badge/badge.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormFieldComponent,
+    InputRefDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule,
+    IconModule,
+    DirectiveModule,
+    AvatarModule,
+    DividerModule,
+    CheckboxModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
